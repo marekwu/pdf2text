31da40bcb402c17f48d6495e7c506aefe1720ed5
Marek Witkowski <marek.witkowski.priv@gmail.com>
2018-09-18 12:53:27 +0200

    Some changes in GUI.

--- src/main/java/net/zepsuty/Main.java
+++ src/main/java/net/zepsuty/Main.java
@@ -1,13 +1,20 @@
 package net.zepsuty;
 
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.io.RandomAccessFile;
+import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+
+import static java.awt.FlowLayout.LEFT;
 
 public class Main
 {
@@ -23,15 +30,29 @@ public class Main
     {
         
         JFrame frame = new JFrame("PDF to TXT Converter");
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+        JPanel panelMain = new JPanel();
+        panelMain.setLayout(new BoxLayout(panelMain, BoxLayout.PAGE_AXIS));
         
-        JLabel titleLab = new JLabel("PDF to TXT Converter");
+        JPanel panelSourceFile = new JPanel();
+        panelSourceFile.setLayout(new FlowLayout(LEFT));
+        
+        JPanel panelDestinationFile = new JPanel();
+        panelDestinationFile.setLayout(new FlowLayout(LEFT));
+        
+        JPanel panelNagitation = new JPanel();
+        panelNagitation.setLayout(new FlowLayout(LEFT));
+        
+        JButton btnConvert = new JButton("Convert");
+        btnConvert.setAlignmentX(Component.LEFT_ALIGNMENT);
         
         JButton btnExit = new JButton("Exit");
-        JButton btnConvert = new JButton("Convert");
+        btnExit.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        
         JButton btnOpenSourceFile = new JButton("Open PDF from file...");
-        JButton btnSaveDetinationFile = new JButton("Save TXT file as...");
+        btnOpenSourceFile.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        
+        JButton btnSaveDestinationFile = new JButton("Save TXT file as...");
+        btnSaveDestinationFile.setAlignmentX(Component.RIGHT_ALIGNMENT);
         
         JLabel sourceFileLabel = new JLabel("PDF to convert:");
         JLabel destinationFileLabel = new JLabel("File to save:");
@@ -42,19 +63,27 @@ public class Main
         JTextField destinationFileTextFiled = new JTextField();
         destinationFileTextFiled.setColumns(50);
         
-        panel.add(titleLab);
-        panel.add(sourceFileLabel);
-        panel.add(sourceFileTextFild);
-        panel.add(btnOpenSourceFile);
-        panel.add(destinationFileLabel);
-        panel.add(destinationFileTextFiled);
-        panel.add(btnSaveDetinationFile);
-        panel.add(btnConvert);
-        panel.add(btnExit);
+        panelSourceFile.add(sourceFileLabel);
+        panelSourceFile.add(sourceFileTextFild);
+        panelSourceFile.add(btnOpenSourceFile);
+        
+        panelDestinationFile.add(destinationFileLabel);
+        panelDestinationFile.add(destinationFileTextFiled);
+        panelDestinationFile.add(btnSaveDestinationFile);
+        
+        panelNagitation.add(Box.createHorizontalGlue());
+        panelNagitation.add(btnConvert);
+        panelNagitation.add(btnExit);
+        
+        panelMain.add(panelSourceFile);
+        panelMain.add(panelDestinationFile);
+        panelMain.add(panelNagitation);
+        panelMain.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
         
         btnOpenSourceFile.addActionListener(e -> {
             
             final JFileChooser fc = new JFileChooser();
+            fc.addChoosableFileFilter(new FileNameExtensionFilter("PDF files", "pdf"));
             int returnVal = fc.showDialog(null, "Open PDF file");
             
             if(returnVal == JFileChooser.APPROVE_OPTION)
@@ -67,9 +96,10 @@ public class Main
             
         });
         
-        btnSaveDetinationFile.addActionListener(e -> {
+        btnSaveDestinationFile.addActionListener(e -> {
             
             final JFileChooser fc = new JFileChooser();
+            fc.addChoosableFileFilter(new FileNameExtensionFilter("TXT files", "txt"));
             int returnVal = fc.showDialog(null, "Save file as TXT");
             
             if(returnVal == JFileChooser.APPROVE_OPTION)
@@ -82,38 +112,45 @@ public class Main
             
         });
         
-        btnConvert.addActionListener(e -> {
-            
-                                         try
-                                         {
-                                             PDDocument pdDocument = PDDocument.load(new File(sourceFileTextFild.getText()));
-                                             String extractedText = new PDFTextStripper().getText(pdDocument);
-                
-                                             PrintWriter pw = new PrintWriter(new FileWriter(destinationFileTextFiled.getText()));
-                                             pw.write(extractedText);
-                                             pw.close();
-                                         }
-                                         catch(IOException e1)
-                                         {
-                                             JOptionPane.showMessageDialog(null, e1);
-                                         }
-            
-                                         JOptionPane.showMessageDialog(null, "Conversion finished.");
-                                     }
-                                    );
+        btnConvert.addActionListener(e -> convertDoc(sourceFileTextFild.getText(), destinationFileTextFiled.getText()));
         
-        btnExit.addActionListener(e ->
-                                      System.exit(0)
+        btnExit.addActionListener(e -> System.exit(0));
         
-                                 );
-        
-        frame.add(panel);
+        frame.add(panelMain);
         frame.setLocation(500, 500);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         
         frame.setVisible(true);
+        
+    }
+    
+    private static void convertDoc(String sourceFile, String descFile)
+    {
+        
+        try
+        {
+            File file = new File(sourceFile);
+            String extractedText;
+            PDFParser parser = new PDFParser(new RandomAccessFile(file, "r"));
+            parser.parse();
+            
+            COSDocument cosDoc = parser.getDocument();
+            PDFTextStripper pdfStripper = new PDFTextStripper();
+            PDDocument pdDoc = new PDDocument(cosDoc);
+            extractedText = pdfStripper.getText(pdDoc);
+            
+            PrintWriter pw = new PrintWriter(new FileWriter(descFile));
+            pw.write(extractedText);
+            pw.close();
+            
+            JOptionPane.showMessageDialog(null, "Conversion finished.");
+        }
+        catch(IOException e1)
+        {
+            JOptionPane.showMessageDialog(null, e1);
+        }
         
     }
     
