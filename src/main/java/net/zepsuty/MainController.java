623be06026b6805d97932a1a975db9495f86dd6d
Marek Witkowski <marek.witkowski.priv@gmail.com>
2018-10-12 09:14:29 +0200

    Added MainController class.

---
+++ src/main/java/net/zepsuty/MainController.java
@@ -0,0 +1,42 @@
+package net.zepsuty;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainController
+{
+    @FXML
+    private void handleMnuOpenSourceFile(ActionEvent actionEvent)
+    {
+    }
+    
+    @FXML
+    private void handleMnuSaveDestinationFile(ActionEvent actionEvent)
+    {
+    }
+    
+    @FXML
+    private void handleExit(ActionEvent actionEvent)
+    {
+    }
+    
+    @FXML
+    private void handleMnuAbout(ActionEvent actionEvent)
+    {
+    }
+    
+    @FXML
+    private void handleBtnOpenSourceFile(ActionEvent actionEvent)
+    {
+    }
+    
+    @FXML
+    private void handleBtnOpenDestinationFile(ActionEvent actionEvent)
+    {
+    }
+    
+    @FXML
+    private void handleBtnConvertFile(ActionEvent actionEvent)
+    {
+    }
+}
